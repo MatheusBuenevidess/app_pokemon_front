484ab615a103d48d80ff953065002bc2c0fe1805
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2020-12-03 14:32:16 -0300

    Front

---
+++ src/components/FindPokemon.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div>
+        <p>{{pokemon}}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'FindPokemons',
+    data () {
+        return {
+        pokemon: null
+        }
+    },
+  mounted () {
+    this.getIdPokemon()
+  },
+  methods: {
+      getIdPokemon(){
+          const {id} = this.$route.params;
+          console.log(id);
+          this.getDados(id);
+      },
+      getDados(id){
+           axios
+            .get(`http://127.0.0.1:8000/api/pokemon/buscar/${id}`)//Adicionar o ID do Pokemon
+            .then(response => (this.pokemon = response))
+      }
+  },
+}
+</script>
---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div>
+        <div v-for="(pokemon,index) in pokemons['data']" :key="pokemon.name">
+            <div class="">
+                <p>{{pokemon.name}}</p>
+                <button @click="detailsPokemon(index+1)">Botao</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ListPokemons',
+    data () {
+        return {
+        pokemons: null
+        }
+    },
+  mounted () {
+    axios
+      .get('http://127.0.0.1:8000/api/pokemon/buscar')
+      .then(response => (this.pokemons = response)) // passar methods
+  },
+  methods: {
+      detailsPokemon(id){
+          this.$router.push(`/detalhes/${id}`)
+      }
+  },
+}
+</script>
