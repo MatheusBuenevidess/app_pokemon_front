1bafeb402278b6bb3f3c1e4ba0edd0a8f8da3686
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2020-12-04 14:48:09 -0300

    att

---
+++ src/components/FindPokemon.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <div>
+        <h4>{{pokemon.name}}</h4>
+        <p>Weight: {{pokemon.weight}}</p>
+        <p>Height: {{pokemon.height}}</p>
+        
+        <button @click="returnHome">Voltar</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'FindPokemons',
+    data () {
+        return {
+        pokemon: null
+        }
+    },
+    mounted () {
+        this.getIdPokemon()
+        console.log(process.env.BASE_URL);
+    },
+    methods: {
+        getIdPokemon(){
+            const {id} = this.$route.params;
+            this.getDados(id);
+        },
+        getDados(id){
+            axios
+            .get(process.env.BASE_URL+ id)
+            .then(response => (this.pokemon = response.data))
+            
+        },
+        returnHome(){
+            this.$router.push(`/`)
+        }
+    },
+}
+</script>
---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div>
+        <div v-for="(pokemon,index) in pokemons" :key="pokemon.name">
+            <div class="">
+                <p>{{pokemon.name}}</p>
+                <button @click="detailsPokemon(index+1)">Detalhes</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ListPokemons',
+    data () {
+        return {
+        pokemons: null
+        }
+    },
+    mounted (){
+        axios
+        .get(process.env.BASE_URL)
+        .then(response => (this.pokemons = response.data)) 
+    },
+    methods: {
+        detailsPokemon(id){
+            this.$router.push(`/detalhes/${id}`)
+        }
+    },
+}
+</script>
